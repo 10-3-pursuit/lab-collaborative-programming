719d3cef05b32efafd28f42cf23c88e954e64b60
hjmclellan <hjmclellan@gmail.com>
2023-10-03 15:06:26 -0400

    Isiah & Harleigh

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const morseCodeDictionary = require("./morse-code-dictionary.json");
  * @param {String[]}  - An array of strings.
  * @returns {string[]} An array of strings.
  */
-function sortByStringLength() {}
+function sortByStringLength(arr) {
+// A function to parse the orginal array and return it sorted.
+// must be sort by length.
+return arr.sort((a,b) => a.length - b.length)
+}
 
 /**
  * Returns an array of the word in all scrolling positions.
@@ -14,14 +18,40 @@ function sortByStringLength() {}
  * Example: "Hello"
  * [ 'elloH', 'lloHe', 'loHel', 'oHell', 'Hello' ]
  */
-function textScroller() {}
+function textScroller(word) {
+  // needs to iterate through a string , moving postion 0 to the end each time .
+  // needs to return the orginal string 
+  if(word.length === 0) {
+    return [];
+  }
+
+  const result = [];
+  for (let i = 1; i < word.length + 1; i++) {
+    // we are slicng 
+    const scrolledWord = word.slice(i) + word.slice(0, i)
+    
+    result.push(scrolledWord);
+  }
+  
+  return result;
+}
 
 /**
  * Returns the difference between the largest and smallest number in the array
  * @param {Number[]} numbers - An array of numbers.
  * @returns {Number} The difference between the largest and smallest number.
  */
-function betweenExtremes() {}
+function betweenExtremes(arr) {
+  // iterate through a an array of numbers looking for min and max.
+  // should subtract the min from the max .
+  if(arr.every((num) => typeof num === 'number')) {
+    const min = Math.min(...arr)
+    const max = Math.max(...arr)
+    return  max - min;
+  }else {
+    return arr;
+  }
+}
 
 /**
  * Returns the difference between the largest and smallest number in the array
@@ -31,7 +61,19 @@ function betweenExtremes() {}
  * Example: "A new month"
  * .- / -. . .-- / -- --- -. - ....
  */
-function morseCodeTranslator() {}
+function morseCodeTranslator(message, dictionary) {
+//  needs to iterate through a string replacing each letter with it's morse code equivalent.
+// spaces need to be replaced with a slash
+const words = message.split(' ')
+// looking at each word 
+const morse = words.map((word) => 
+  word 
+  .split('')
+  .map((char) => dictionary[char.toUpperCase()])
+  .join(' ')
+);
+  return morse.join(' ');
+}
 
 module.exports = {
   sortByStringLength,
