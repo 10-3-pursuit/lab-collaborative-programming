6fcb365abfc39b0ab56c4af63e4953de80330f20
brenda-soto <bsoto@pursuit.org>
2023-10-04 09:14:04 -0400

    All functions work

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ const morseCodeDictionary = require("./morse-code-dictionary.json");
  * @param {String[]}  - An array of strings.
  * @returns {string[]} An array of strings.
  */
-function sortByStringLength() {}
+
+
+//return an array of strings in ascending order by length
+
+//add param to function (array of strings)
+//sort through the array to put shortest word first, increasing to longest word last 
+//return sorted array
+const sortByStringLength = array => array.sort((word1, word2) => word1.length - word2.length)
 
 /**
  * Returns an array of the word in all scrolling positions.
@@ -14,14 +21,48 @@ function sortByStringLength() {}
  * Example: "Hello"
  * [ 'elloH', 'lloHe', 'loHel', 'oHell', 'Hello' ]
  */
-function textScroller() {}
+
+//Returns an array of each iteration of a word each time the first letter is removed and added on to the end of the word.
+
+function textScroller(word) {
+//create an empty 
+// const scrollerArray = []
+//split the given string into an array of letters, save to variable
+// const lettersArray = 
+//remove the letter at index 0 (using shift) and add it to the end of the string array (using push)
+return word.split('').reduce((acc, _, index) => {       //splitting the string provided to an array of letters. reduce word to an array of words of the shifted result (index + 1 times)
+  const lettersToShift = word.substring(0, index + 1)   //assigning a variable to the substring we need to shift to the end, based on current index (+ 1 because the 2nd param in substring is not inclusive)
+  const lettersToKeep = word.substring(index + 1) //assigning a variable to the substring we need to keep, based on current index (+ 1, with no second param because it will grab everything to the end)
+  acc[index] = `${lettersToKeep}${lettersToShift}`
+  return acc
+}, [])
+//join the letters without a space
+//push new word to created array
+//return created array
+
+// 'hello'
+// ['h', 'e', 'l', 'l', 'o']
+// const movedLetter = letters.shift()
+// letters = ['e', 'l', 'l', 'o']
+// movedLetter = h
+// letters.push(movedLetter)
+// letters = ['e', 'l', 'l', 'o', 'h']
+// const newWord = letters.join('') => 'elloh'
+// scrollerArray.push(newWord)
+}
 
 /**
  * Returns the difference between the largest and smallest number in the array
  * @param {Number[]} numbers - An array of numbers.
  * @returns {Number} The difference between the largest and smallest number.
  */
-function betweenExtremes() {}
+const betweenExtremes = numbers => {
+  if(!numbers.every(num => typeof num === 'number')) {
+    return numbers
+  } else {
+  return Math.max(...numbers) - Math.min(...numbers)
+  }
+}
 
 /**
  * Returns the difference between the largest and smallest number in the array
@@ -31,7 +72,8 @@ function betweenExtremes() {}
  * Example: "A new month"
  * .- / -. . .-- / -- --- -. - ....
  */
-function morseCodeTranslator() {}
+const morseCodeTranslator = (message, dictionary) => 
+  message.split(' ').map(word => word.split('').map(character => dictionary[character.toUpperCase()]).join(' ')).join(' ')
 
 module.exports = {
   sortByStringLength,
